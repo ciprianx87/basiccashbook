93e721228a8bc30acea0d8401142aa6f09936197
ciprianx87 <ciprianx87@1d84d7f7-9089-283b-36a7-a258f27c8409>
2013-03-30 14:44:56 +0000

    initial commit

---
+++ trunk/CashBook/CashBook.Controls/CashBookWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashBook.Controls
+{
+    /// <summary>
+    /// Interaction logic for CashBook.xaml
+    /// </summary>
+    public partial class CashBookWindow : UserControl
+    {
+        public CashBookWindow()
+        {
+            InitializeComponent();
+            this.Loaded += CashBook_Loaded;
+        }
+
+        void CashBook_Loaded(object sender, RoutedEventArgs e)
+        {
+            CashBooks = new ObservableCollection<Data.Model.CashBook>();
+            dataGrid.ItemsSource = CashBooks;
+        }
+
+        public ObservableCollection<Data.Model.CashBook> CashBooks { get; set; }
+
+        private void dataGrid_KeyUp_1(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                CashBooks.Insert(CashBooks.Count, new Data.Model.CashBook() { NrCrt = CashBooks.Count });
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            CashBooks.Add(new Data.Model.CashBook());
+
+        }
+    }
+}
---
+++ trunk/CashBook/CashBook/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using CashBook.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CashBook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            ChangeContent(ContentTypes.CashBook);
+            
+        }
+
+        private void ChangeContent(object contentType)
+        {
+            ContentTypes type = (ContentTypes)contentType;
+            switch (type)
+            {
+                case ContentTypes.CashBook:
+                    contentControl.Content = new CashBook.Controls.CashBookWindow();
+                    break;
+                case ContentTypes.MainContent:
+                    contentControl.Content = new CashBook.Controls.MainContent();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
